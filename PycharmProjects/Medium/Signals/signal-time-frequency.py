74527e2fa25bf150fa5d6814db3b68d760d81ef3
georgeginis <georgeginis@sailinternet.com>
2024-02-09 15:29:07 -0800

    Skeleton script

---
+++ PycharmProjects/Medium/Signals/signal-time-frequency.py
@@ -0,0 +1,22 @@
+########################################################################################################################
+#
+# Python code for demonstrating signal representation in the time-domain and in the frequency-domain
+# Uses the numpy and matplotlib libraries to achieve functionality similar to MATLAB
+#
+########################################################################################################################
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Example of a continuous-time signal
+# Pulse with duration of T_p
+
+
+
+t = np.linspace(0, 10, 100)
+plt.plot(x, np.sin(x))
+
+plt.show()
+
